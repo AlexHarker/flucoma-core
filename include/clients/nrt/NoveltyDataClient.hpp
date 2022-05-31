af89b302afb2d9ba82b8d20a363b72f0b5ac6b97
Alex Harker <ajharker@gmail.com>
2022-05-31 12:16:55 +0100

    Update to deal with latency and attributes correctly

--- include/clients/nrt/NoveltyDataClient.hpp
+++ include/clients/nrt/NoveltyDataClient.hpp
@@ -58,8 +58,15 @@ public:
     
   static constexpr auto& getParameterDescriptors() { return NoveltyDataParams; }
 
+  index latency()
+  {
+    index filterSize = get<kFilterSize>();
+    if (filterSize % 2) filterSize++;
+    return ((get<kKernelSize>() + 1) >> 1) + (filterSize >> 1);
+  }
+    
   template<typename T>
-  Result process(FluidContext& c)
+  Result process(FluidContext&)
   {
     if(!get<kSource>().get())
       return {Result::Status::kError, "No input buffer supplied"};
@@ -77,41 +84,62 @@ public:
 
         if(!idx.exists())
             return {Result::Status::kError, "Output buffer not found"};
-
     }
 
-//    if(!idx.valid())
-//        return {Result::Status::kError, "Can't access output buffer"};
-
-    size_t nChannels = get<kNumChans>()  == -1 ? source.numChans() : get<kNumChans>();
-    size_t nFrames   = get<kNumFrames>() == -1 ? source.numFrames(): get<kNumFrames>();
-
+    index numFrames = get<kNumFrames>() == -1
+      ? (source.numFrames() - get<kOffset>())
+      : get<kNumFrames>();
+    index numChannels = get<kNumChans>() == -1
+      ? (source.numChans() - get<kStartChan>())
+      : get<kNumChans>();
+      
     index kernelSize = get<kKernelSize>();
     index filterSize = get<kFilterSize>();
       
     algorithm::NoveltyFeature processor(kernelSize, filterSize);
 
-    auto inputData = FluidTensor<double,1>(nChannels);
-    auto outputData = FluidTensor<double,1>(nFrames);
+    auto inputData = FluidTensor<double,1>(numChannels);
+    auto outputData = FluidTensor<double,1>(numFrames);
       
-    processor.init(kernelSize, filterSize, nChannels);
-    
-    for (size_t i = 0; i < nFrames; i++)
+    processor.init(kernelSize, filterSize, numChannels);
+          
+    index preRoll = latency();
+    index chanOffset = get<kStartChan>();
+    index offset = get<kOffset>();
+      
+    auto getInput = [&](index idx)
     {
-        // Get col/row
+        // FIXME: make this nicer
 
-        // FIX - make this nicer
-        
-        for (size_t j = 0; j < nChannels; j++)
-            inputData(j) = source.samps(get<kOffset>() + i, 1, get<kStartChan>() + j)(0);
-        
+        for (index j = 0; j < numChannels; j++)
+            inputData(j) = source.samps(idx, 1, chanOffset + j)(0);
+    };
+      
+    for (index i = 0; i < preRoll; i++)
+    {
+        getInput(offset + i);
+        processor.processFrame(inputData);
+    }
+      
+    offset += preRoll;
+      
+    for (index i = 0; i < numFrames - preRoll; i++)
+    {
+        getInput(offset + i);
         outputData(i) = processor.processFrame(inputData);
     }
+
+    // FIXME: consider if there should be a "default input"
+      
+    inputData.fill(0.0);
+
+    for (index i = numFrames - preRoll; i < numFrames; i++)
+        outputData(i) = processor.processFrame(inputData);
     
     BufferAdaptor::Access idx(get<kOutput>().get());
         
     Result resizeResult =
-    idx.resize(nFrames, 1, source.sampleRate());
+    idx.resize(numFrames, 1, source.sampleRate());
     if (!resizeResult.ok()) return resizeResult;
         
     idx.samps(0) <<= outputData;
